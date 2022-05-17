cc09dc50dd7f02687e8d7b8ab29989cacd654622
Paul Vautier <paul.vautier@kosmos.fr>
2022-05-17 16:32:22 +0200

    Added project

---
+++ src/main/java/org/filmt/projetagile/ProjetAgileApplication.java
@@ -0,0 +1,13 @@
+package org.filmt.projetagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAgileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAgileApplication.class, args);
+	}
+
+}
