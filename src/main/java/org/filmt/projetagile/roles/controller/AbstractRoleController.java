a35b31e03ed07a15b2b41b5bd36458eccce4ad86
Paul <vautier.paul14350@gmail.com>
2022-06-24 10:25:10 +0200

    ajout des roles propriétaire pour ajouter et supprimer un role

--- src/main/java/org/filmt/projetagile/roles/controller/AbstractRoleController.java
+++ src/main/java/org/filmt/projetagile/roles/controller/AbstractRoleController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.filmt.projetagile.exception.RoleNotFoundException;
 import org.filmt.projetagile.roles.model.Role;
 import org.filmt.projetagile.roles.service.RoleService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,11 +34,13 @@ public class AbstractRoleController<T> {
         return roleService.getRoleForEntity(userName, entityId).orElseThrow(()-> new RoleNotFoundException(String.format("No role was found for user '%s' at entity '%s'", userName, entityId)));
     }
 
+    @PreAuthorize("schoolPrivilegesHigherThan(#groupId, 'OWNER')")
     @DeleteMapping(value = "", params = {"user_name", "entityId"})
     public void removeRole(@RequestParam(name = "user_name") final String userName, @RequestParam final String entityId) {
         roleService.removeRole(userName, entityId);
     }
 
+    @PreAuthorize("schoolPrivilegesHigherThan(#groupId, 'OWNER')")
     @PostMapping(value = "", params = {"user_name", "entityId", "role"})
     public void changeRole(@RequestParam(name = "user_name") String userName, @RequestParam final String entityId, @RequestParam final Role role) {
         roleService.changeRole(userName, entityId, role);
